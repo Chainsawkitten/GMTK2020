4e2fbda39d89b71a27e9fd14bd6d7aa8508a120f
Daniel Eliasson <catscratch.d@gmail.com>
2020-07-11 12:19:57 +0200

    Added GameObject and Text classes

---
+++ scripts/game_object.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name GameObject
+
+export var is_pushable: bool = true
+
+#func _init():
+#	pass
+
+#func _ready():
+#	pass
+
+#func _process(delta):
+#	pass
---
+++ scripts/text.gd
@@ -0,0 +1,24 @@
+extends GameObject
+class_name Text
+
+
+# enum Action {
+# 	MOVE_UP,
+# 	MOVE_LEFT,
+# 	MOVE_RIGHT,
+# 	MOVE_DOWN,
+# 	DIE,
+# 	WIN,
+# 	SIZE
+# }
+
+export var action: int = 0
+
+#func _init():
+#	pass
+
+#func _ready():
+#	pass
+
+#func _process(delta):
+#	pass
