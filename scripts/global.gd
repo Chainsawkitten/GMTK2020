0d75e6b35efa02ecf37924afd675a75812ff77c8
Albin Bernhardsson <catscratchesp@gmail.com>
2020-07-11 14:23:03 +0200

    Add Global enums

---
+++ scripts/global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# The types of game objects there are.
+enum GameObjectType {
+	TEXT
+}
+
+# The types of text there are.
+enum TextType {
+	BUTTON_UP,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_DOWN,
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_X,
+	BUTTON_Y,
+	IS,
+	DIE
+}
