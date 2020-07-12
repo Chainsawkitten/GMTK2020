4cebc54f77a94638d9a4efb7ac96e86739329646
Albin Bernhardsson <catscratchesp@gmail.com>
2020-07-12 19:30:54 +0200

    Poof sound

--- scripts/barrier.gd
+++ scripts/barrier.gd
@@ -7,16 +7,17 @@ extends GameObject
 export var levels_to_beat : int = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	if LevelHandler.get_beaten_levels() == levels_to_beat:
-		# Create a poof!
-		var poof = load("scenes/effects/poof.tscn").instance()
-		poof.position = position + Vector2(Global.cell_size / 2, Global.cell_size / 2)
-		get_parent().call_deferred("add_child", poof)
-		
-		kill()
-	elif LevelHandler.get_beaten_levels() > levels_to_beat:
-		kill()
+func _process(_delta):
+	if !TurnHandler.paused:
+		if LevelHandler.get_beaten_levels() == levels_to_beat:
+			# Create a poof!
+			var poof = load("scenes/effects/poof.tscn").instance()
+			poof.position = position + Vector2(Global.cell_size / 2, Global.cell_size / 2)
+			get_parent().call_deferred("add_child", poof)
+			
+			kill()
+		elif LevelHandler.get_beaten_levels() > levels_to_beat:
+			kill()
 
 # Kill the barrier.
 func kill():
